6cacbfbf09917acc0a704b052dff096d054ddd6f
findxc <liuxiaocui1993@gmail.com>
2022-06-05 17:07:19 +0800

    support local swagger.json

--- bin/getSwagger.js
+++ bin/getSwagger.js
@@ -1,6 +1,18 @@
+const path = require('path')
 const axios = require('axios').default
 
 function getSwagger(options) {
+  if (options.path) {
+    const swaggerPath = path.resolve(process.cwd(), options.path)
+    try {
+      return require(swaggerPath)
+    } catch (err) {
+      console.error(err)
+      console.log(`please confirm the path is correct: ${swaggerPath}`)
+    }
+    return
+  }
+
   return axios
     .get(options.url)
     .then(res => {
